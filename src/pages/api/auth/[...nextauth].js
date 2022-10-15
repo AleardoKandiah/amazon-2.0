0ee13d687c63c2cefca0f4ca056820dd3dfad400
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-10-15 14:01:02 +0100

    google sign in

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import FacebookProvider from "next-auth/providers/facebook";
 
 export default NextAuth({
     // Confugure one or more authentification providers
@@ -9,10 +8,7 @@ export default NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
-        FacebookProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }),
+
 
         //...add providers here
     ],
