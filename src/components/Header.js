e792b63fda4c8c0666c49b858ea4249fd8041f65
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-10-15 18:56:41 +0100

    Router to pagse

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,12 @@ import {
     ShoppingCartIcon,
     } from "@heroicons/react/outline"
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter} from 'next/router';
+
 
 function Header() {
     const {data: session, status} = useSession();
+    const router = useRouter();
 
     return (
     <header>
@@ -17,6 +20,7 @@ function Header() {
         <div className="flex items-center bg-amazon_blue p-1 flex-row py-2">
             <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
                 <Image
+                onClick={() => router.push('/')}
                 src="https://links.papareact.com/f90"s
                 width={150}
                 height={40}
@@ -47,7 +51,7 @@ function Header() {
                     <p className="font-extrabold md:text-sm">& Orders</p>
                 </div>
                 {/* Basket div */}
-                <div className="relative link flex items-center">
+                <div onClick={() => router.push('/checkout')} className="relative link flex items-center">
                     <span className="absolute top-0 right-0 rounded-full text-black font-bold md:right-10 h-4 w-4 bg-yellow-400 text-center">0</span>
 
                     <ShoppingCartIcon className="h-10"/>
