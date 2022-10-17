45019253bd90e2d28292f1d8f73ce16ef9bb850a
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-10-17 11:40:38 +0100

    checkout page

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import basketReducer from "../slices/basketSlice";
 
+// the globas store setup
 export const store = configureStore({
   reducer: {
     basket: basketReducer,
