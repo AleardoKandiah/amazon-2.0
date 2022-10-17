6f04c20d332766ef2cd41befbb7e35023e4703a1
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-10-17 17:31:41 +0100

    checkout button

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -3,9 +3,12 @@ import CheckoutProduct from "../components/CheckoutProduct"
 import Image from "next/image";
 import { useSelector } from "react-redux";
 import { selectItems } from "../slices/basketSlice";
+import Currency from "react-currency-formatter";
+import { useSession } from "next-auth/react";
 
 function Checkout() {
   const items = useSelector(selectItems);
+  const {data: session, status} = useSession();
 
   return (
     <div className="bg-gray-100">
@@ -46,7 +49,19 @@ function Checkout() {
 
           {/* Right */}
           <div>
+            {items.length > 0 && (
+              <>
+                <h2 className="whitespace-nowrap">
+                    Subtotal ({items.length} items):
+                    <span className="font-bold">
 
+                    </span>
+                </h2>
+                <button className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'}`}>
+                  {!session ? "Sign in to checkout" : "Proceed to checkout}"}
+                </button>
+              </>
+            )}
           </div>
 
         </main>
