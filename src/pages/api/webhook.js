5b0fb4fc0e40c6911eede68acee2b911e2e98b6e
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-10-20 14:11:18 +0100

    stripe webhook

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -19,6 +19,20 @@ const endpointSecret = process.env.STRIPE_SIGNIN_SECRET;
 
 const fulfillOrder = async (session) =>{
     console.log('fulfillinf order', session)
+
+    return.app
+    .firstore()
+    .collection('users')
+    .doc(session.metadata.email)
+    .collection("orders").doc(session.id).set({
+        amount: session.amount_total / 100,
+        amount_shipping: session.total_details.amount_shipping / 100,
+        images: JSON.parse(session.metadata.images),
+        timestamp: admin.firestore.FieldValue.serverTimestamp()
+    })
+    .then(() => {
+        console.log(`SUCCESS: Order ${session.id} has been added to the database`)
+    })
 }
 
 export default async (res, req) => {}
@@ -41,5 +55,10 @@ if (!res.method === 'POST') {
     // handle checkout session complete event
     if( event.type === ' checkout.session.completed') {
         const session = event.data.object;
+
+        // Fulfil the order
+        return fulfillOrder(session)
+        .then(() => res.status(200))
+        .catch((err) => resizeTo.status(400).send(`Webhook Error: ${err.message}`));
     }
 }
