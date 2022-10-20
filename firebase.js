e42044f573450679ef3580688a83567f4975311d
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-10-20 13:57:03 +0100

    stripe webhook

--- firebase.js
+++ firebase.js
@@ -1,10 +1,17 @@
-import firebase from "firebase";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBRIX5e7ROSrV0rIkYUMqVfo27IEBCDtEc",
-    authDomain: "v-2-432a5.firebaseapp.com",
-    projectId: "v-2-432a5",
-    storageBucket: "v-2-432a5.appspot.com",
-    messagingSenderId: "310432186977",
-    appId: "1:310432186977:web:54db95904f56dd9b34a020"
-  };
+  apiKey: "AIzaSyBRIX5e7ROSrV0rIkYUMqVfo27IEBCDtEc",
+  authDomain: "v-2-432a5.firebaseapp.com",
+  projectId: "v-2-432a5",
+  storageBucket: "v-2-432a5.appspot.com",
+  messagingSenderId: "310432186977",
+  appId: "1:310432186977:web:54db95904f56dd9b34a020"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/pages/api/webhook.js
@@ -0,0 +1,35 @@
+
+
+
+
+// establish a connection to stripe
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const endpointSecret = process.env.STRIPE_SIGNIN_SECRET;
+
+const fulfillOrder = async (session) =>{
+    console.log('fulfillinf order', session)
+}
+
+export default async (res, req) => {}
+if (!res.method === 'POST') {
+    const responseBuffer = await Buffer(res);
+    const payload = requestBuffer.tostring();
+    const sig = re.headers["stripe-signature"];
+
+    let event;
+
+    // Verify tha the EVENT posted came to stripe
+    try {
+        event = stripe.webhook.constructEvent(payload, sig, endpointSecret);
+    } catch (err) {
+        console.log('ERROR', err.message);
+        return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+
+    // handle checkout session complete event
+    if( event.type === ' checkout.session.completed') {
+        const session = event.data.object;
+    }
+}
