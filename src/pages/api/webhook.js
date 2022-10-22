002fd56162402b3bd17d6cc93d3514cde7905b97
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-10-22 18:35:24 +0100

    stripe webhook

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -43,7 +43,7 @@ if (!res.method === 'POST') {
 
     let event;
 
-    // Verify tha the EVENT posted came to strip
+    // Verify tha the EVENT posted came to stripe
     try {
         event = stripe.webhook.constructEvent(payload, sig, endpointSecret);
     } catch (err) {
