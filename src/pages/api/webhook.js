e06a5eaf116dc9c2d071783ab33c49e46bec407c
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-11-01 15:37:10 +0000

    stripe webhook

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -12,7 +12,7 @@ const app = !isAdmin.apps.length
     :
     isAdmin.apply();
 
-// Establish a connection to stripe  s
+// Establish a connection to stripe  
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const endpointSecret = process.env.STRIPE_SIGNIN_SECRET;
